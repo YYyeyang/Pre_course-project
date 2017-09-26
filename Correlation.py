d0a9ef1c7e3baa52624aaa883d1405f365731ead
YYyeyang <32057874+YYyeyang@users.noreply.github.com>
2017-09-26 16:43:59 +0200

    started with numpy

--- Correlation.py
+++ Correlation.py
@@ -1,7 +1,21 @@
 import build_dic
+import numpy
 
 age={}
+unemployment={}
 
-q=build_dic.build('Avg_age_processed.csv',age)
+age_dic=build_dic.build('Avg_age_processed.csv',age)
+unemployment_dic=build_dic.build('Registered_unemployment_processed.csv',unemployment)
 
-print(q)
+print(age_dic)
+print(unemployment_dic)
+
+result=numpy.corrcoef(age_dic[2013],unemployment_dic[2013])[0, 1]
+print(result)
+
+
+#loop over the year
+#loop over the distrct
+#append to my two list
+
+
