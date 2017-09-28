425ad2d629223b9ff825f29b4ac4007af9ab150c
YYyeyang <32057874+YYyeyang@users.noreply.github.com>
2017-09-28 14:59:41 +0200

    calculated now for all years

--- Correlation.py
+++ Correlation.py
@@ -1,20 +1,32 @@
 import build_and_convert
 import numpy
+import math
 import matplotlib.pyplot as plt
 
+# built full-dictionary for age and unemployment
 age_dic= build_and_convert.build('Avg_age_processed.csv')
 unemployment_dic= build_and_convert.build('Registered_unemployment_processed.csv')
 
-print(age_dic)
-print(unemployment_dic)
-
+# convert them into a float list per year per type
 age2013=build_and_convert.convert(age_dic,2013)
-print(age2013)
+age2014=build_and_convert.convert(age_dic,2014)
+age2015=build_and_convert.convert(age_dic,2015)
+age2016=build_and_convert.convert(age_dic,2016)
 
 unemployment2013=build_and_convert.convert(unemployment_dic,2013)
+unemployment2014=build_and_convert.convert(unemployment_dic,2014)
+unemployment2015=build_and_convert.convert(unemployment_dic,2015)
+unemployment2016=build_and_convert.convert(unemployment_dic,2016)
 
-sig2013 = numpy.corrcoef(age2013,unemployment2013)[0,1]
-print(sig2013)
+# add all year together
+all_age=age2013+age2014+age2015+age2016
+all_unemployment=unemployment2013+unemployment2014+unemployment2015+unemployment2016
 
-plt.scatter(age2013,unemployment2013)
+# calculate the correlation coefficient
+cor = numpy.corrcoef(all_age,all_unemployment)[0,1]
+print(cor)
+
+# plot my correlations
+plt.scatter(all_age,all_unemployment)
 plt.show()
+
